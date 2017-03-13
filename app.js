2f24133d036a90325599a9cf5b63c6d81c21a17c
Cam Shotter and Marcus Crane <dev+cam-shotter+marcus-crane@hashrocket.com>
2017-03-14 12:19:58 +1300

    Messaging works and starting to add Twitter

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const steam = require('steam')
+const Steam = require('steam')
+const Twitter = require('twitter')
 const env = require('dotenv').config()
 
 const steamClient = new Steam.SteamClient();
+const steamUser = new Steam.SteamUser(steamClient);
 const steamFriends = new Steam.SteamFriends(steamClient);
+
+const client = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
 
 steamClient.connect();
 steamClient.on('connected', function() {
@@ -17,4 +26,13 @@ steamClient.on('logOnResponse', function(logonResp) {
     console.log('Logged in!');
     steamFriends.setPersonaState(Steam.EPersonaState.Online); // to display your bot's status as "Online"
   }
+});
+
+steamFriends.on('message', function(source, message, type, chatter) {
+    if (type === 1) {
+        steamFriends.sendMessage(source, 'Look for your message at https://twitter.com/spinyswordfish')
+    }
+  // if (message == 'ping') {
+  //   steamFriends.sendMessage(source, 'pong', Steam.EChatEntryType.ChatMsg); // ChatMsg by default
+  // }
 });
