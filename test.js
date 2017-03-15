4334cbba2e742468ea04b959d4ddbdfbf9d0b311
Marcus Crane <marcus@thingsima.de>
2017-03-15 14:50:40 +1300

    Changed test env back to the original

--- test.js
+++ test.js
@@ -3,10 +3,10 @@ const env = require('dotenv').config()
 const cheerio = require('cheerio')
 
 const client = new Twitter({
-  consumer_key: process.env.ALT_CONSUMER_KEY,
-  consumer_secret: process.env.ALT_CONSUMER_SECRET,
-  access_token_key: process.env.ALT_ACCESS_TOKEN,
-  access_token_secret: process.env.ALT_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
 let devices = {}
